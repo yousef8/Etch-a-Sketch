52bc19b3ff1c24703702ab846cbdab14b1b71c6c
yousef ghonim <yousef4511@outlook.com>
2022-07-30 21:58:14 +0200

    Fix the JS function to actually print 16x16 grid

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ function makeGrid() {
   const gridContainer = document.createElement("div");
   gridContainer.classList.add("grid-container");
 
-  for (let i = 1; i <= 4; i++) {
-    for (let j = 1; j <= 4; j++) {
+  for (let i = 1; i <= 16; i++) {
+    for (let j = 1; j <= 16; j++) {
       const div = document.createElement("div");
       div.setAttribute("data-row", i);
       div.setAttribute("data-col", j);
